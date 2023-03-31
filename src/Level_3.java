449877fd16a308fc07fe1b799c74183594b654f9
TheBestGuigui <117637202+TheBestGuigui@users.noreply.github.com>
2023-03-31 16:09:16 +0200

    Un peu d'histoire

--- src/Level_3.java
+++ src/Level_3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Level_3 {
     public static void level3(Wizard wizard) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\n" + "\nYou engage in a fierce battle with them !");
+        System.out.println("\nAfter the second year of study at Hogwarts, the long awaited summer holidays are finally here. You decide to go swimming at the lake in the Forbidden Forest." + "\nWhile swimming, you are attacked by Dementors." + "\nYou engage in a fierce battle with them !");
         Enemy enemy = Enemy.Dementor;
         Character.Fight_enemy(wizard, enemy);
     }
