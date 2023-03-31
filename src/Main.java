093dc17037a0fc08f48f0627c4bd4ff3931583d1
TheBestGuigui <117637202+TheBestGuigui@users.noreply.github.com>
2023-03-31 17:22:35 +0200

    Fin

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        while (true) {
+        boolean Jeu = true;
+        while (Jeu) {
             new Game().StartGame();
+            Jeu = false;
         }
     }
 }
